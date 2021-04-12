c290cc05fa71e174e00d1fc3a5e5db37b425d22a
Ah Siu <ahsiu04@gmail.com>
2021-04-12 17:54:00 +0800

    random game

--- random_game.py
+++ random_game.py
@@ -1,14 +1,18 @@
 import random
 r = random.randint(1, 100)
-#print(r)
+print(r)
+count = 0
 
 while True:
     num = input('Guess number(1-100): ')
     num = int(num)
+    count += 1
     if num < r:
         print('Higher')
     elif num > r:
         print('Lower')
     else:
         print('you got it')
+        print('This is your ', count, ' try')
         break
+    print('This is your ', count, ' try')
